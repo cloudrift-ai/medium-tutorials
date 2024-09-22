0c00c5f714902344d706a62c280829123a7dbf74
Dmitry Trifonov <slonegg@gmail.com>
2024-09-22 12:36:19 -0700

    implement LLM logic

--- 01-prompt-engineering/app.py
+++ 01-prompt-engineering/app.py
@@ -1,8 +1,35 @@
+import ollama
 import gradio as gr
 
 
 def generate_question(text):
-    return "What is the topic of this text?", ["A", "B", "C"], "A"
+    prompt = f"""{text}"""
+    context = ollama.generate(model='llama3.1', prompt=prompt)['context']
+
+    prompt = f"""
+    Generate a question and several answer choices based on the aforementioned text.
+    Separate the question and answer choices with a newline.
+    Use asterisk to denote the correct answer.
+    Do not provide any hints, explanations, or additional information.
+    """
+    response = ollama.generate(model='llama3.1', prompt=prompt, context=context)['response']
+    print("LLM Response:\n", response)
+
+    # Parse the response
+    lines = response.split('\n')
+    question = lines[0]
+    choices = []
+    correct_answer = 0
+    for i, line in enumerate(lines[1:]):
+        line = line.strip()
+        if line == '':
+            continue
+        if '*' in line:
+            correct_answer = len(choices)
+            line = line.replace('*', '')
+        choices.append(line)
+
+    return question, choices, correct_answer
 
 
 text = None
@@ -28,7 +55,7 @@ with gr.Blocks() as demo:
         else:
             return {
                 answer_selector: gr.Radio(label=question, choices=choices, visible=True),
-                result_lbl: gr.Markdown("## Correct" if answer == correct_answer else "## Incorrect", visible=True),
+                result_lbl: gr.Markdown("## Correct" if answer == choices[correct_answer] else "## Incorrect", visible=True),
             }
 
     submit_btn.click(
